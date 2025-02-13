c04542a607c3a096b88fcc6dfd119569e0a7a265
Mannat Garg <mannatgarg2006@gmail.com>
2025-02-13 10:41:37 +0530

    Practice-assignment-1

--- index.js
+++ index.js
@@ -34,14 +34,32 @@
 
 const express = require('express');
 const { resolve } = require('path');
+const data = require('./data.json');
 
 const app = express();
 const port = 3010;
+app.use(express.json())
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.post('/students/above-threshold' , (req,res)=>{
+  const threshold = req.body.threshold;
+  if(typeof threshold !== 'number'){
+    return res.status(400).json({message: "Bad Request"})
+  }
+
+  const filterStudent = data.filter(ele => ele.total > threshold);
+  res.status(201).send({
+    count : filterStudent.length,
+    students : filterStudent.map((item)=> ({
+      name : item.name,
+      total : item.total
+    })),
+  });
 });
 
 app.listen(port, () => {
